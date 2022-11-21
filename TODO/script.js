a8a5bae1fae22a74d0eb2c15efa4ebecb52d1014
Alex <kurilets.o.v@gmail.com>
2022-11-21 15:45:27 +0200

    To Do V2

--- TODO/script.js
+++ TODO/script.js
@@ -1,7 +1,11 @@
 let todolist = [];
+let tododone = [];
+let list = todolist;
 const todolistNode = document.querySelector('.js_todolist');
 const inputNode = document.querySelector('.js_input');
 const btnNode = document.querySelector('.add_btn');
+const donebtnNode = document.querySelector('.done_btn_list');
+donebtnNode.innerHTML = `DONE`;
 
 function add(text) {
     const todoitem = {
@@ -16,21 +20,35 @@ function remove(id) {
     todolist.forEach(todoitem => {
         if (todoitem.id === id) {
             todoitem.status = true;
+            tododone.push(todoitem);
         };
     });
 };
 
-function output() {
+function output(list) {
     let html = '';
-    todolist.forEach(todoitem => {
-        if (todoitem.status === true) {
-            return;
-        };
-        html +=
-            `
-            <div class="todoitem"><button class="done_btn" data-id='${todoitem.id}'></button>${todoitem.text}</<div>
+    if (list === todolist) {
+        todolist.forEach(todoitem => {
+            if (todoitem.status === true) {
+                return;
+            };
+            html +=
+                `
+                <div class="todoitem"><button class="done_btn" data-id='${todoitem.id}'></button>${todoitem.text}</<div>
+            `;
+        });
+    }
+    else {
+        tododone.forEach(todoitem => {
+            if (todoitem.status === false) {
+                return;
+            }
+            html +=
+                `
+            <div class="doneitem">${todoitem.text}</<div>
         `;
-    });
+        })
+    }
     todolistNode.innerHTML = html;
 };
 
@@ -43,7 +61,7 @@ btnNode.addEventListener('click', () => {
     };
 
     add(text);
-    output();
+    output(list);
     inputNode.value = '';
 });
 
@@ -53,5 +71,21 @@ todolistNode.addEventListener('click', (event) => {
     };
     const id = event.target.dataset.id;
     remove(id);
-    output();
+    output(list);
 });
+
+donebtnNode.addEventListener('click', () => {
+    if (document.getElementById('switch_list').classList.contains('done_btn_list')) {
+        list = tododone;
+        document.getElementById('switch_list').classList.remove('done_btn_list');
+        document.getElementById('switch_list').classList.add('todo_btn_list');
+        donebtnNode.innerHTML = `TO DO`;
+    }
+    else {
+        list = todolist;
+        document.getElementById('switch_list').classList.remove('todo_btn_list');
+        document.getElementById('switch_list').classList.add('done_btn_list');
+        donebtnNode.innerHTML = `DONE`;
+    }
+    output(list);
+})
